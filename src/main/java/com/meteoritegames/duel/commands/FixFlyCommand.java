0ac56502708dbd8201e06d57d21aef467c1cec95
Giovanni D <gsdambrosio@gmail.com>
2023-02-05 12:25:08 -0800

    Allow commands to be used outside of duel

--- src/main/java/com/meteoritegames/duel/commands/FixFlyCommand.java
+++ src/main/java/com/meteoritegames/duel/commands/FixFlyCommand.java
@@ -17,72 +17,66 @@ public class FixFlyCommand implements CommandClass {
 	}
 
 	@Command(name="fix",
-			description="Fix items ONLY IN DUEL")
+			description="Fix items")
 	public void fix(CommandSender sender) {
 		if (!(sender instanceof Player)) return;
 		Player p = (Player) sender;
 
 		Duel d = plugin.playerIsInDuel(p);
-		if (d == null) {
-			p.sendMessage(plugin.getText("not-in-duel"));
-			return;
+		if (d != null) {
+			if (!d.getDuelArgs().get(9).isEnabled()) {
+				p.sendMessage(plugin.getText("not-enabled").replace("%command%", "/fix"));
+				return;
+			}
 		}
 
-		if (d.getDuelArgs().get(9).isEnabled()) {
-			ItemStack item = p.getInventory().getItemInHand();
-			item.setDurability((short) 0);
-			p.getInventory().setItemInHand(item);
-			p.playSound(p.getLocation(), Sound.ANVIL_USE, 1, 1);
-			p.updateInventory();
-		} else {
-			p.sendMessage(plugin.getText("not-enabled").replace("%command%", "/fix"));
-		}
+		ItemStack item = p.getInventory().getItemInHand();
+		item.setDurability((short) 0);
+		p.getInventory().setItemInHand(item);
+		p.playSound(p.getLocation(), Sound.ANVIL_USE, 1, 1);
+		p.updateInventory();
 	}
 
 	@Command(name="fix",
-			description="Fix all items ONLY IN DUEL",
+			description="Fix all items",
 			args="all")
 	public void fixAll(CommandSender sender) {
 		if (!(sender instanceof Player)) return;
 		Player p = (Player) sender;
 
 		Duel d = plugin.playerIsInDuel(p);
-		if (d == null) {
-			p.sendMessage(plugin.getText("not-in-duel"));
-			return;
-		}
-
-		if (d.getDuelArgs().get(10).isEnabled()) {
-			for (int i = 0; i < p.getInventory().getSize(); i++) {
-				ItemStack item = p.getInventory().getItem(i);
-				if (item == null) continue;
-				item.setDurability((short) 0);
-				p.getInventory().setItem(i, item);
+		if (d != null) {
+			if (!d.getDuelArgs().get(9).isEnabled()) {
+				p.sendMessage(plugin.getText("not-enabled").replace("%command%", "/fix all"));
+				return;
 			}
-
-			p.playSound(p.getLocation(), Sound.ANVIL_USE, 1, 1);
-			p.updateInventory();
-		} else {
-			p.sendMessage(plugin.getText("not-enabled").replace("%command%", "/fix all"));
 		}
+
+		for (int i = 0; i < p.getInventory().getSize(); i++) {
+			ItemStack item = p.getInventory().getItem(i);
+			if (item == null) continue;
+			item.setDurability((short) 0);
+			p.getInventory().setItem(i, item);
+		}
+
+		p.playSound(p.getLocation(), Sound.ANVIL_USE, 1, 1);
+		p.updateInventory();
 	}
 
 	@Command(name="fly",
-			description="Fly ONLY IN DUEL")
+			description="Fly")
 	public void fly(CommandSender sender) {
 		if (!(sender instanceof Player)) return;
 		Player p = (Player) sender;
 
 		Duel d = plugin.playerIsInDuel(p);
-		if (d == null) {
-			p.sendMessage(plugin.getText("not-in-duel"));
-			return;
+		if (d != null) {
+			if (!d.getDuelArgs().get(9).isEnabled()) {
+				p.sendMessage(plugin.getText("not-enabled").replace("%command%", "/fly"));
+				return;
+			}
 		}
 
-		if (d.getDuelArgs().get(11).isEnabled()) {
-			p.setAllowFlight(!p.getAllowFlight());
-		} else {
-			p.sendMessage(plugin.getText("not-enabled").replace("%command%", "/fly"));
-		}
+		p.setAllowFlight(!p.getAllowFlight());
 	}
 }
